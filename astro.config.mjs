34ef5e3c735ba9a9627d515ede3d17fae7a9520a
protonk <protonk@gmail.com>
2026-04-17 17:02:36 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import { globSync } from 'glob'
+
+const site = 'https://adampunk.com'
+const assetPages = globSync('public/{documents,presentations}/*.pdf').map(
+  (p) => `${site}/${p.replace(/^public\//, '')}`,
+)
 
 export default defineConfig({
-  site: 'https://protonk.github.io',
+  site,
+  integrations: [sitemap({ customPages: assetPages })],
 })
